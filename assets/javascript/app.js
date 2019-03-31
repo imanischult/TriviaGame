04593758130342419569b035d58efda749356a72
Imani Schulterbrandt <imani.vicente@outlook.com>
2019-03-30 22:46:39 -0400

    Final game logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,134 +1,188 @@
-//Create an array of objects to hold your questions for the trivia game
+$(document).ready(function(){
+  
+    // event listeners
+    $("#remaining-time").hide();
+    $("#start").on('click', trivia.startGame);
+    $(document).on('click' , '.option', trivia.guessChecker);
+    
+  })
+  
 
-var questionArray = [
-    {
-        question: "What is Earth's largest continent?",
-        choiceA: "Antarctica",
-        choiceB: "Africa",
-        choiceC: "Asia",
-        choiceD: "North America",
-        correctAnswer: "C"
+  var trivia = {
+
+    correct: 0,
+    incorrect: 0,
+    unanswered: 0,
+    currentSet: 0,
+    timer: 20,
+    timerOn: false,
+    timerId : '',
+
+    // The following uses objects to hold questions and answers for game
+    questions: {
+      q1: 'What chicken wing restaurant is Atlanta known for? ',
+      q2: `Which of the following airlines have made Atlanta it's hub?`,
+      q3: 'Which restaurant do consumers enjoy after late night events?',
+      q4: 'What year did Atlanta host the Olympics?',
+      q5: "Which three schools make up the Atlanta University Center?",
+      q6: `What is the name of Atlanta's metro system?`,
+      q7: "Which two freeways make up the Spaghetti Junction?"
     },
-    {
-        question: "What country has the most natural lakes?",
-        choiceA: "United States", 
-        choiceB: "Canada",
-        choiceC: "Australia",
-        choiceD: "Greenland",
-        correctAnswer: "Canada"
+    options: {
+      q1: ['Wings Spot', 'JR Crickets', 'Wing Depot', 'Buffalo Wild Wings'],
+      q2: ['United Airlines', 'American Airlines', 'Delta Air Lines', 'Spirit Airlines'],
+      q3: ['Waffle House', 'IHOP', `McDonald's`, 'Cook Out'],
+      q4: ['1988', '1994', '1996', '2002'],
+      q5: ['Georgia State University, Georgia Tech, and UGA','Emory, Georgia State University, and Spelman College','Spelman College, Morehouse College, and Clark Atlanta University','Morehouse College, Georgia State University, and Emory'],
+      q6: ['MARTA','Atlanta Metro System','Metro ATL','MATL'],
+      q7: ['I-75 and I-85', 'I-85 and I-20', 'I-20 and I-285','I-285 and I-75']
     },
-    {
-        question: "What is the driest place on Earth?",
-        choiceA: "Sahara Desert",
-        choiceB: "McMurdo Dry Valley, Antarctica",
-        choiceC: "Death Valley, California",
-        choiceD: "Arabian Desert",
-        correctAnswer: "McMurdo Dry Valley, Antarctica"
+    answers: {
+      q1: 'JR Crickets',
+      q2: 'Delta Air Lines',
+      q3: 'Waffle House',
+      q4: '1996',
+      q5: 'Spelman College, Morehouse College, and Clark Atlanta University',
+      q6: 'MARTA',
+      q7: 'I-285 and I-75'
+    },
+    
+    // *****  MAIN GAME LOGIC ***** //
+    
+    // method to initialize game
+    startGame: function(){
+      // restarting game results
+      trivia.currentSet = 0;
+      trivia.correct = 0;
+      trivia.incorrect = 0;
+      trivia.unanswered = 0;
+      clearInterval(trivia.timerId);
+      
+      // show game section
+      $('#game').show();
+      
+      //  empty last results
+      $('#results').html('');
+      
+      // show timer
+      $('#timer').text(trivia.timer);
+      
+      // remove start button
+      $('#start').hide();
+  
+      $('#remaining-time').show();
+      
+      // ask first question
+      trivia.nextQuestion();
+      
+    },
+    // method to loop through and display questions and options 
+    nextQuestion : function(){
+      
+      // set timer to 10 seconds for each question
+      trivia.timer = 10;
+       $('#timer').removeClass('last-seconds');
+      $('#timer').text(trivia.timer);
+      
+      // to prevent timer speed up
+      if(!trivia.timerOn){
+        trivia.timerId = setInterval(trivia.timerRunning, 1000);
+      }
+      
+      // gets all the questions then indexes the current questions
+      var questionContent = Object.values(trivia.questions)[trivia.currentSet];
+      $('#question').text(questionContent);
+      
+      // an array of all the user options for the current question
+      var questionOptions = Object.values(trivia.options)[trivia.currentSet];
+      
+      // creates all the trivia guess options in the html
+      $.each(questionOptions, function(index, key){
+        $('#options').append($('<button class="option btn btn-info btn-lg">'+key+'</button>'));
+      })
+      
+    },
+    // method to decrement counter and count unanswered if timer runs out
+    timerRunning : function(){
+      // if timer still has time left and there are still questions left to ask
+      if(trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.questions).length){
+        $('#timer').text(trivia.timer);
+        trivia.timer--;
+          if(trivia.timer === 4){
+            $('#timer').addClass('last-seconds');
+          }
+      }
+      // the time has run out and increment unanswered, run result
+      else if(trivia.timer === -1){
+        trivia.unanswered++;
+        trivia.result = false;
+        clearInterval(trivia.timerId);
+        resultId = setTimeout(trivia.guessResult, 1000);
+        $('#results').html('<h3>Out of time! The answer was '+ Object.values(trivia.answers)[trivia.currentSet] +'</h3>');
+      }
+      // if all the questions have been shown end the game, show results
+      else if(trivia.currentSet === Object.keys(trivia.questions).length){
+        
+        // adds results of game (correct, incorrect, unanswered) to the page
+        $('#results')
+          .html('<h3>Thank you for playing!</h3>'+
+          '<p>Correct: '+ trivia.correct +'</p>'+
+          '<p>Incorrect: '+ trivia.incorrect +'</p>'+
+          '<p>Unaswered: '+ trivia.unanswered +'</p>'+
+          '<p>Please play again!</p>');
+        
+        // hide game sction
+        $('#game').hide();
+        
+        // show start button to begin a new game
+        $('#start').show();
+      }
+      
+    },
+    // method to evaluate the option clicked
+    guessChecker : function() {
+      
+      // timer ID for gameResult setTimeout
+      var resultId;
+      
+      // the answer to the current question being asked
+      var currentAnswer = Object.values(trivia.answers)[trivia.currentSet];
+      
+      // if the text of the option picked matches the answer of the current question, increment correct
+      if($(this).text() === currentAnswer){
+        // turn button green for correct
+        $(this).addClass('btn-success').removeClass('btn-info');
+        
+        trivia.correct++;
+        clearInterval(trivia.timerId);
+        resultId = setTimeout(trivia.guessResult, 1000);
+        $('#results').html('<h3>Correct Answer!</h3>');
+      }
+      // else the user picked the wrong option, increment incorrect
+      else{
+        // turn button clicked red for incorrect
+        $(this).addClass('btn-danger').removeClass('btn-info');
+        
+        trivia.incorrect++;
+        clearInterval(trivia.timerId);
+        resultId = setTimeout(trivia.guessResult, 1000);
+        $('#results').html('<h3>Better luck next time! '+ currentAnswer +'</h3>');
+      }
+      
+    },
+    // method to remove previous question results and options
+    guessResult : function(){
+      
+      // increment to next question set
+      trivia.currentSet++;
+      
+      // remove the options and results
+      $('.option').remove();
+      $('#results h3').remove();
+      
+      // begin next question
+      trivia.nextQuestion();
+       
     }
-];
-
-// ******************** CREATE GLOBAL VARIABLES ******************** //
-
-
-// REFERENCES TO THE DOM //
-
-const $start = $("#start");
-const $triviaGame = $("#triviaGame");
-const $question = $("#question");
-const $qImg= $("#qImg");
-const $choiceA = $("#A");
-const $choiceB = $("#B");
-const $choiceC = $("#C");
-const $choiceD = $("#D");
-const $counter = $("#counter");
-const $bTimeGauge = $("#bTimeGauge");
-const $timeGauge = $("#timeGauge");
-const $progress = $("#progress");
-const $score = $("#score");
-
-// Global variables 
-const lastQuestionIndex = questionArray.length - 1;
-var runningQuestionIndex = 0;
-
-
-// Render questions
-
-function renderQuestion () {
-    let userQuestion = questionArray[runningQuestionIndex];
-
-    $question.text(userQuestion.question);
-    $choiceA.text(userQuestion.choiceA);
-    $choiceB.text(userQuestion.choiceB);
-    $choiceC.text(userQuestion.choiceC);
-    $choiceD.text(userQuestion.choiceD);
-
-    $start.style.display = "none";
-    renderQuestion();
-    $triviaGame.style.display = "block"
-
-}
-
-console.log(renderQuestion());
-// var questionTime = 15
-
-// var gaugeWidth = 150;
-
-// var count = 0;
-
-// var gaugeProgressUnit = gaugeWidth/questionTime;
-
-// var timer = setInterval(counterRender,1000);
-
-
-
-// // ******************** CREATE GLOBAL FUNCTIONS ******************** //
-
-
-// // This function will render our next question
-// function renderQuestion () {
-//     q = questionArray[runningQuestionIndex];
-//     // Write everything to html //
-//     }
-
-
-// // This function will check if we have a next question or not. If we do, then we will proceed; if we don't, then the timer is cleared and the users score is rendered
-
-// var checkNextQuestion = function() {
-//     if (runningQuestionIndex < lastQuestionIndex) {
-//         count = 0;
-//         runningQuestionIndex++;
-//         renderQuestion(); // don't forget to create this function
-    
-//     } else {
-//         clearInterval(timer); // don't forget this one either
-//         scoreRender();
-//     }
-// }
-
-
-// // This function holds the game logic for the counter, or the timer. It sets guidelines on how long the user has to answer the question, and what happens if that time runs out //
-// function counterRender() {
-//     if (count <= questionTime ) {
-//         counterRender.innerHTML = count;
-//         timGauge.style.width = gaugeProgressUnit * count + "px" ;
-//         count++;
-    
-//     } else {
-//         answerIsWrong(); // Don't forget to create this function!
-//         checkNextQuestion(); // Created! :)
-//     }
-
-// }
-
-
-// // This function checks for the correct answer //
-// function checkAnswer (answer) {
-//     if (questionArray[runningQuestionIndex].correct == correctAnswer) {
-//         answerIsCorrect(); // Create a function for this 
-    
-//     } else {
-//         answerIsWrong(); // Create a function for this
-//         checkNextQuestion();
-//     }
-// }
-
+  
+  }
